cdeb50a17993df5510c5faa101d37a0095c7114b
Abigail Trapp <abbsthepandahead@gmail.com>
2022-07-02 21:27:32 -0600

    initiating GET update

--- code.js
+++ code.js
@@ -1,10 +1,14 @@
 var mykey = config.MY_KEY
 const url = `https://api.openweathermap.org/data/2.5/weather?q=denver&appid=${mykey}`
-
 const xhr = new XMLHttpRequest()
 xhr.open('GET',url)
 xhr.send()
 xhr.onload = () =>{
     const data = JSON.parse(xhr.response)
-    console.log(data)
+let userCity = document.querySelector('input').value
+let button = document.querySelector('button').addEventListener('click', ()=>{
+    userCity = data.name
+    console.log(userCity)
+})
+    
 }
